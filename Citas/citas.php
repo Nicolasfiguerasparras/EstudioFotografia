982bbb27f4a807956a47ab90e2d0f754c7bf693b
Nicolás Figueras Parras Desktop <nicolasfiguerasparras@gmail.com>
2019-01-20 11:38:52 +0100

    Update citas.php

--- Citas/citas.php
+++ Citas/citas.php
@@ -19,19 +19,12 @@
             $year=date("Y");
             $diaActual=date("j");
 
-            // Obtenemos el dia de la semana del primer dia del mes (devuelve 0 para domingo, 6 para sabado)
-            $diaSemana=date("w",mktime(0,0,0,$month,1,$year))+7;
-            // Obtenemos el ultimo dia del mes
-            $ultimoDiaMes=date("d",(mktime(0,0,0,$month+1,1,$year)-1));
-
             // Creamos un array con todos los nombres de los meses del año
             $meses=array(1=>"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
         ?>
         
         <table>
-            <caption>
-                <?php echo $meses[$month]." ".$year?>
-            </caption>
+            
             <tr>
                 <th>Lun</th>
                 <th>Mar</th>
@@ -43,33 +36,41 @@
             </tr>
             <tr bgcolor="silver">
             <?php
-                $last_cell=$diaSemana+$ultimoDiaMes;
-                // hacemos un bucle hasta 42, que es el máximo de valores que puede
-                // haber... 6 columnas de 7 dias
-                for($i=1;$i<=42;$i++){
-                        if($i==$diaSemana)
-                        {
-                                // determinamos en que dia empieza
-                                $day=1;
+                function pintaCalendario(){
+                    echo "<caption>";
+                        echo $meses[$month].' '.$year;
+                    echo "</caption>";
+                    // Obtenemos el dia de la semana del primer dia del mes (devuelve 0 para domingo, 6 para sabado)
+                    $diaSemana=date("w",mktime(0,0,0,$month,1,$year))+7;
+                    // Obtenemos el ultimo dia del mes
+                    $ultimoDiaMes=date("d",(mktime(0,0,0,$month+1,1,$year)-1));
+                    
+                    $last_cell=$diaSemana+$ultimoDiaMes;
+                    // Hacemos un bucle hasta 42 (máximo de valores que puede haber: 6 columnas de 7 dias)
+                    for($i=1;$i<=42;$i++){
+                        if($i==$diaSemana){
+                            // Determinamos en qué día empieza
+                            $day=1;
                         }
-                        if($i<$diaSemana || $i>=$last_cell)
-                        {
-                                // celca vacia
-                                echo "<td>&nbsp;</td>";
+                        if($i<$diaSemana || $i>=$last_cell){
+                            // Celda vacia
+                            echo "<td>&nbsp;</td>";
                         }else{
-                                // mostramos el dia
-                                if($day==$diaActual)
-                                        echo "<td class='hoy'>$day</td>";
-                                else
-                                        echo "<td>$day</td>";
-                                $day++;
+                            // Mostramos el dia
+                            if($day==$diaActual){
+                                echo "<td class='hoy'>$day</td>";
+                            }else{
+                                echo "<td>$day</td>";
+                            }
+                            $day++;
                         }
-                        // cuando llega al final de la semana, iniciamos una columna nueva
-                        if($i%7==0)
-                        {
+                        // Cuando llega al final de la semana, iniciamos una columna nueva
+                        if($i%7==0){
                             echo "</tr><tr>\n";
                         }
                     }
+                }
+                pintaCalendario();
             ?>
             </tr>
         </table>
